270ced50068c13525cc41b7da5a19ca1f0e99c50
Michael Phillips <michael@crushlovely.com>
2014-04-22 16:44:54 -0400

    Test suites for value object

--- test/objects/value.spec.js
+++ test/objects/value.spec.js
@@ -1,34 +1,104 @@
 var Value;
 var value1;
 var value2;
+var spy;
 
 describe('Value', function() {
 
   before( function() {
-    Value = require('../../lib/objects/value');
+    Value = require('../../lib/utensils').Value;
     value1 = new Value(2);
-  })
+  });
 
   describe('#value', function() {
 
     it('returns the correct value', function() {
       expect(value1.value).to.equal(2);
-    })
+    });
 
-  })
+  });
 
   describe('#isEqualTo', function() {
 
-    it('should be false if values are not equal', function() {
-      value2 = new Value(3);
-      expect(value1.isEqualTo( value2 )).to.be.false;
-    })
-
-    it('should be true if values are equal', function() {
+    it('returns true if values are equal', function() {
       value2 = new Value(2);
       expect(value1.isEqualTo( value2 )).to.be.true;
-    })
+    });
 
-  })
+    it('returns false if values are not equal', function() {
+      value2 = new Value(3);
+      expect(value1.isEqualTo( value2 )).to.be.false;
+    });
 
-})
+  });
+
+  describe('#isNotEqualTo', function() {
+
+    it('returns true if values are not equal', function() {
+      value2 = new Value(3);
+      expect(value1.isNotEqualTo( value2 )).to.be.true;
+    });
+
+    it('returns false if values are equal', function() {
+      value2 = new Value(2);
+      expect(value1.isNotEqualTo( value2 )).to.be.false;
+    });
+
+  });
+
+  describe('#isGreaterThan', function() {
+
+    it('returns true if value is greater than other value', function() {
+      value2 = new Value(1);
+      expect(value1.isGreaterThan( value2 )).to.be.true;
+    });
+
+    it('returns false if value is not greater than other value', function() {
+      value2 = new Value(3);
+      expect(value1.isGreaterThan( value2 )).to.be.false;
+    });
+
+  });
+
+  describe('#isLessThan', function() {
+
+    it('returns true if value is less than other value', function() {
+      value2 = new Value(3);
+      expect(value1.isLessThan( value2 )).to.be.true;
+    });
+
+    it('returns false if value is not less than other value', function() {
+      value2 = new Value(1);
+      expect(value1.isLessThan( value2 )).to.be.false;
+    });
+
+  });
+
+});
+
+describe('#create', function() {
+
+  before( function() {
+    Value = require('../../lib/utensils').Value;
+    Value = Value.extend({
+      parse: function( value ) {
+        return value - 1;
+      }
+    });
+    spy = sinon.spy( Value.prototype, 'parse' );
+    value1 = new Value(2);
+  });
+
+  it('calls parse', function() {
+    expect( spy ).to.have.been.calledOnce;
+  });
+
+  it('creates a new instance of Value', function() {
+    expect( value1 ).to.be.an.instanceOf( Value );
+  });
+
+  it('sets the modified value', function() {
+    expect( value1.value ).to.equal(1);
+  });
+
+});
